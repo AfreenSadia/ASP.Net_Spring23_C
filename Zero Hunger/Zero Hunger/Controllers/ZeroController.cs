92dca3a68d08df084fe96c4e6dbbe5ff07cfe1c9
AfreenSadia <84623357+AfreenSadia@users.noreply.github.com>
2023-03-25 01:31:19 +0600

    DB first approach

---
+++ Zero Hunger/Zero Hunger/Controllers/ZeroController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Zero_Hunger.Models;
+
+namespace Zero_Hunger.Controllers
+{
+    public class ZeroController : Controller
+    {
+        // GET: Zero
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult AddRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddRequest(Request R)
+        {
+            var db = new Zero_HungerEntitiess();
+            var n = db.Requests.ToList();
+            db.Requests.Add(R);
+            db.SaveChanges();
+            return View("ViewRequest",n);
+        }        
+        public ActionResult ViewRequest()
+        {
+            var db = new Zero_HungerEntitiess();
+            var R = db.Requests.ToList();
+            return View(R);
+        }
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee E)
+        {
+            var db = new Zero_HungerEntitiess();
+            db.Employees.Add(E);
+            db.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult ViewEmployee()
+        {
+            var db = new Zero_HungerEntitiess();
+            var em = db.Employees.ToList();
+            return View(em);
+        }
+        public ActionResult Assign(Request R)
+        {
+            var db = new Zero_HungerEntitiess();
+            var n = db.Employees.ToList();
+            ViewBag.Id = R.Id;
+            ViewBag.Rname = R.RestaurentName;
+            ViewBag.Fname = R.FoodName;
+            ViewBag.Q = R.Quantity;
+            return View(n);
+        }
+        [HttpPost]
+        public ActionResult Assign(Collection C)
+        {
+            var db = new Zero_HungerEntitiess();
+            db.Collections.Add(C);
+            db.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult Collected()
+        {
+            var db = new Zero_HungerEntitiess();
+            var collections = db.Collections.ToList();
+            return View(collections);
+        }
+       
+    }
+}
